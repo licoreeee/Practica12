585f10c2bdfd087d06a1a82665e32ed0d96e1a64
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2025-04-08 18:17:31 -0700

    proyecto

---
+++ Crud/app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id ("com.google.gms.google-services")
+}
+
+android {
+    namespace = "mx.edu.itson.crud"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "mx.edu.itson.crud"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation (platform("com.google.firebase:firebase-bom:33.12.0"))
+
+    implementation ("com.google.firebase:firebase-firestore-ktx:25.1.3")
+    implementation ("com.google.firebase:firebase-core:21.1.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ Crud/app/src/main/java/mx/edu/itson/crud/Tarea.kt
@@ -0,0 +1,14 @@
+package mx.edu.itson.crud
+
+data class Tarea(
+    var id: String = "",
+    var titulo: String = "",
+    var descripcion: String = "")
+{
+    fun toMap(): Map<String, String> {
+        return mapOf(
+            "titulo" to titulo,
+            "descripcion" to descripcion
+        )
+    }
+}
